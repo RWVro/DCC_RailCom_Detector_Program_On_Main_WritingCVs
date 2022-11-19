69398a95dd0f924ebfac9c45c0604518a31b142c
RWVro <105106146+RWVro@users.noreply.github.com>
2022-11-19 12:46:41 +0100

    Create DCC_RailCom_Detector_Program_On_Main_WritingCVs.ino

---
+++ DCC_RailCom_Detector_Program_On_Main_WritingCVs.ino
@@ -0,0 +1,60 @@
+
+//====================================== DCC RailCom Detector Program ================================
+//
+//
+//=================================  Board = ESP32 Dev Module (NodeMCU ESP32) =========================
+//
+//
+//=================================== ESP32 with L298N motorcontrollermodule =========================
+
+#define RPWM 19                 // to RPWM MotorShield pin 1 & 4
+#define LPWM 18                 // to LPWM MotorShield pin 2 & 3
+
+#include "Detector.h"
+#include "Pom.h"
+
+int loopCount;
+//================================= Setup =======================================
+
+void setup()
+{
+  Serial.begin(500000);
+  delay(3000);
+
+  pinMode(RPWM, OUTPUT);              // to RPWM MotorShield pin 1 & 4
+  pinMode(LPWM, OUTPUT);              // to LPWM MotorShield pin 2 & 3
+
+  digitalWrite(RPWM, LOW);
+  digitalWrite(LPWM, LOW);
+
+  pinMode(railComInt, INPUT);         // GPIO15 input pin  int Raicom Detector
+  pinMode(railComRX, INPUT);          // GPIO16 Connected to RailCom Detector RX
+
+  delay(3000);
+  Serial.println("Program Started!");
+  Serial.println("");
+
+  Poort2.begin(250000, SERIAL_8N1, railComRX, 17);    // Define and start ESP32 serial port
+  delay(3000);
+}
+
+//======================================= Main Program ==============================
+//
+// Do not use delay() commands in the loop program.
+// Otherwise there is a chance that the locomotive will quickly run off the rails.
+// DCC signal then stops temporarily.
+
+void loop()
+{
+  basicNormalPulses(10);          // Create DCC signal
+
+  if (loopCount < 50)
+  {
+    basicNormalPulses(10);
+    CVPomWritePacket();
+    basicNormalPulses(10);
+    CVPomWritePacket();
+    basicNormalPulses(10);
+    loopCount++;
+  }
+}
